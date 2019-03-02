4c95e72ec74bc435dc292c9d77c63cd669bd7ec1
Colin <colin.voisard@yahoo.com>
2019-03-01 22:53:15 -0500

    Initial commit

---
+++ euchre_scoreboard/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  euchre_scoreboard
+//
+//  Created by Colin Voisard on 2019-02-27.
+//  Copyright © 2019 cvoisard. All rights reserved.
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+
+}
